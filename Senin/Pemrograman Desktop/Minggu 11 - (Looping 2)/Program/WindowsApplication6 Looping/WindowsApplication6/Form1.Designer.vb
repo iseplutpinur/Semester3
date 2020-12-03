7521df954fecfe9030f3068fd682514c44a062d6
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2020-12-03 17:40:08 +0700

    Update minggu 10

--- Senin/Pemrograman Desktop/Minggu 11 - (Looping 2)/Program/WindowsApplication6 Looping/WindowsApplication6/Form1.Designer.vb
+++ Senin/Pemrograman Desktop/Minggu 11 - (Looping 2)/Program/WindowsApplication6 Looping/WindowsApplication6/Form1.Designer.vb
@@ -76,7 +76,7 @@ Partial Class Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 176)
+        Me.ClientSize = New System.Drawing.Size(320, 227)
         Me.Controls.Add(Me.cmdpangkatfibo)
         Me.Controls.Add(Me.cmdfibo)
         Me.Controls.Add(Me.cmdpangkat)
