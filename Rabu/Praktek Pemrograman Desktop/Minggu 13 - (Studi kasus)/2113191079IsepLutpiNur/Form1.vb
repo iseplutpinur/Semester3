8253c0d45a112c6db22c31968fb6a514eac5f24c
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2020-12-20 09:31:30 +0700

    update minggu 13

---
+++ Rabu/Praktek Pemrograman Desktop/Minggu 13 - (Studi kasus)/2113191079IsepLutpiNur/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private supliers() As String = {"PT. MAKMUR BERSAMA", _
+                              "PT. ALIM UNTUNG", _
+                              "MULYA UTAMA SEJAHTERA", _
+                              "PT. INDAH MEKAR MULYA", _
+                              "CV. SUMBER UTAMA BERKAH"}
+
+
+
+    Private Sub btnKeluar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        End
+    End Sub
+End Class
---
+++ Rabu/Praktek Pemrograman Desktop/Minggu 13 - (Studi kasus)/2113191079IsepLutpiNur/Module1.vb
@@ -0,0 +1,22 @@
+﻿Imports MySql.Data.MySqlClient
+
+Module koneksimysql
+    Public conn As MySqlConnection
+    Public cmd As MySqlCommand
+    Public rd As MySqlDataReader
+    Public da As MySqlDataAdapter
+
+    Sub koneksi()
+        Try
+            Dim str As String
+            str = "Server=localhost;Port=3306;userid=root;password=123;database=warungsaya"
+            conn = New MySqlConnection(Str)
+            If conn.State = ConnectionState.Closed Then
+                conn.Open()
+            End If
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+End Module
