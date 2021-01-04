aba2e1cafd6c2745ed39c74719f2ec70b91d18b9
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-01-04 17:05:39 +0700

    Update minggu 14

---
+++ Kamis/Interaksi Manusia Dan Komputer/Minggu 14 - (Quiz 2)/tugas/Project/WinFormsApp1/WinFormsApp1/Form1.Designer.vb
@@ -0,0 +1,229 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DateTimePicker1 = New System.Windows.Forms.DateTimePicker()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'DateTimePicker1
+        '
+        Me.DateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom
+        Me.DateTimePicker1.Location = New System.Drawing.Point(137, 160)
+        Me.DateTimePicker1.Name = "DateTimePicker1"
+        Me.DateTimePicker1.Size = New System.Drawing.Size(106, 23)
+        Me.DateTimePicker1.TabIndex = 0
+        Me.DateTimePicker1.Value = New Date(2021, 1, 4, 23, 55, 0, 0)
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(130, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(239, 25)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Form Pendaftaran Anggota"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(24, 110)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(90, 15)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Nama Lengkap:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 139)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(75, 15)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "Tempat Lahr:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(24, 192)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 15)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "Alamat:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(24, 168)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(80, 15)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "Tanggal Lahir:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(24, 81)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(37, 15)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "NPM:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(24, 284)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 15)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Jenis Kelamin:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(137, 73)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(189, 23)
+        Me.TextBox1.TabIndex = 8
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(137, 189)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(309, 67)
+        Me.TextBox2.TabIndex = 9
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(137, 131)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(143, 23)
+        Me.TextBox3.TabIndex = 10
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(137, 102)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(309, 23)
+        Me.TextBox4.TabIndex = 11
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Checked = True
+        Me.RadioButton1.Location = New System.Drawing.Point(137, 280)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(72, 19)
+        Me.RadioButton1.TabIndex = 12
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Laki-Laki"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(240, 280)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(86, 19)
+        Me.RadioButton2.TabIndex = 13
+        Me.RadioButton2.Text = "Perempuan"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(370, 337)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 35)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Batal"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(266, 337)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 35)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Submit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(468, 389)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DateTimePicker1)
+        Me.Name = "Form1"
+        Me.Text = "Nama Lengkap"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+End Class
---
+++ Kamis/Interaksi Manusia Dan Komputer/Minggu 14 - (Quiz 2)/tugas/Project/WinFormsApp1/WinFormsApp1/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        DateTimePicker1.Format = DateTimePickerFormat.Custom
+        Const V As String = "dd/MM/yyy"
+        DateTimePicker1.CustomFormat = V
+
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.ForeColor = ColorTranslator.FromHtml("#0275d8")
+        Button1.FlatAppearance.BorderColor = ColorTranslator.FromHtml("#0275d8")
+
+
+        Button2.FlatStyle = FlatStyle.Flat
+        Button2.ForeColor = ColorTranslator.FromHtml("#ffffff")
+        Button2.FlatAppearance.BorderColor = ColorTranslator.FromHtml("#0275d8")
+        Button2.BackColor = ColorTranslator.FromHtml("#0275d8")
+    End Sub
+
+    Private Sub Luncurkan()
+        MsgBox("NPM harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("NPM harus di isi angka saja", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("NPM harus dari 10 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+        MsgBox("Nama Lengkap harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Nama Lengkap minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+        MsgBox("Nama Lengkap Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",
+               vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Tempat Lahir harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Tempat Lahir minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+        MsgBox("Tempat Lahir Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",
+               vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Tanggal Lahir harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Alamat harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Alamat minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+        MsgBox("Alamat Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",
+               vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        MsgBox("Jenis Kelamin Harus di pilih", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        End
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Call Luncurkan()
+    End Sub
+End Class
