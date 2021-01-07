708326b672ba00a7172acf3d5ae08115b66d93da
Isep Lutpi Nur <iseplutpinur7@gmail.com>
2021-01-07 15:05:32 +0700

    Update minggu 14

--- Kamis/Interaksi Manusia Dan Komputer/Minggu 14 - (Quiz 2)/tugas/Project/WinFormsApp1/WinFormsApp1/Form1.vb
+++ Kamis/Interaksi Manusia Dan Komputer/Minggu 14 - (Quiz 2)/tugas/Project/WinFormsApp1/WinFormsApp1/Form1.vb
@@ -1,4 +1,4 @@
-﻿Public Class Form1
+Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         DateTimePicker1.Format = DateTimePickerFormat.Custom
         Const V As String = "dd/MM/yyy"
@@ -16,23 +16,34 @@
     End Sub
 
     Private Sub Luncurkan()
-        MsgBox("NPM harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("NPM harus di isi angka saja", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("NPM harus dari 10 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
-        MsgBox("Nama Lengkap harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Nama Lengkap minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
-        MsgBox("Nama Lengkap Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",
-               vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Tempat Lahir harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Tempat Lahir minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
-        MsgBox("Tempat Lahir Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",
-               vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Tanggal Lahir harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Alamat harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Alamat minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
-        MsgBox("Alamat Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",
-               vbOKOnly + MessageBoxIcon.Error, "Peringatan")
-        MsgBox("Jenis Kelamin Harus di pilih", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+        
+		MsgBox("NPM harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("NPM harus di isi angka saja", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("NPM harus dari 10 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+
+		MsgBox("Nama Lengkap harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Nama Lengkap minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+
+		MsgBox("Nama Lengkap Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Tempat Lahir harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Tempat Lahir minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+
+		MsgBox("Tempat Lahir Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Tanggal Lahir harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Alamat harus di isi", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Alamat minimal 3 dgit", vbOKOnly + MessageBoxIcon.Information, "Perhatian")
+
+		MsgBox("Alamat Harus berupa huruf tidak boleh simbol atau angka kecuali min, titik, koma dan tanda petik tunggal ",vbOKOnly + MessageBoxIcon.Error, "Peringatan")
+
+		MsgBox("Jenis Kelamin Harus di pilih", vbOKOnly + MessageBoxIcon.Error, "Peringatan")
 
     End Sub
 
---
+++ Selasa/Kewirausahaan/Minggu 13 - 6 Thinking Hats/Resume Video 6 Topi Berpikir.md
@@ -0,0 +1,31 @@
+Nama: Isep Lutpi Nur
+
+NPM: 2113191079
+
+Tugas: Minggu 13 - 6 Thinking Hats
+
+<hr>
+
+## Resume Video 6 Topi Berpikir
+
+Manusia diberi akal dan fikiran oleh tuhan, berfikir adalah daya jiwa yang dapat meletakan hubungan-hubungan antara pengetahuan kita, berfikir merupakan proses yang dialektis yaitu selama kita berpikir pikiran kita berada dalam tanya jawa untuk dapat meletakan hubungan pengetahuan kita.
+
+
+
+Ilmu berfikir sangat penting dalam mengambil keputusan utnuk menghasilkan keputusan yang cepat dan tepat serta mampu menganalisa dengan lebih luas jernih jelas dan objektif,
+
+
+
+Berpikir sebagai "Suatu KETERAMPILAN untuk mendayagunakan KECERDASAN berdasarkan pengalaman", Dr. Edward de Bono (Psikiolog, Penulis buku. Six Thinking Hats 1985).
+
+
+
+Menurut Edward de Bono Manusia memiliiki 6 gaya berpikir di ibaratkan 6 buah topi, beliau merupakan seorang pakar terkemuka yang mengajarkan KETERAMPILAN BERFIKIR dan penggagas konsep berpikir lateral yang kemudian mengembangkan teknik teknik formal tersebut menjadi teknik kreatif.
+
+
+
+![image-20210107125232871](D:\Kampus\Semester3\Selasa\Kewirausahaan\Minggu 13 - 6 Thinking Hats\image-20210107125232871.png)
+
+
+
+ 
